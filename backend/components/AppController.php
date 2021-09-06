288b3ff6776a367055b0e350b90a052065361210
Yurii Kovalchuk <zt1552@gmail.com>
2021-09-06 06:22:24 +0300

    50% home page

---
+++ backend/components/AppController.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace backend\components;
+
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+
+}
--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,14 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'asset/css/vendor/vendor.min.css',
+        'asset/css/plugins/plugins.min.css',
+        'asset/css/style.min.css'
     ];
     public $js = [
+        'asset/js/vendor/vendor.min.js',
+        'asset/js/plugins/plugins.min.js',
+        'asset/js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
