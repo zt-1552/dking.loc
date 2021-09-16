4fff27c72b2efa804377e5c4a8d7d6c4a0194f47
Yurii Kovalchuk <zt1552@gmail.com>
2021-09-16 06:15:01 +0300

    home page with day offers 3

--- frontend/controllers/HomeController.php
+++ frontend/controllers/HomeController.php
@@ -11,11 +11,9 @@ class HomeController extends AppController
 
     public function actionIndex()
     {
-<<<<<<< HEAD
+
         $offers = Product::find()->where(['is_offer' => 1])->limit(4)->all();
-=======
-        $offers = Product::find()->where(['is_offer' => 1])->limit(2)->all();
->>>>>>> origin/main
+
 //        debug($offers);
 
         return $this->render('index', compact('offers'));
